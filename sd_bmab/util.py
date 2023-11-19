e1ed989692154b99f4d5a5bc89e1f21872249063
portu-sim <safecode@naver.com>
2023-11-19 15:55:10 +0900

    * GroudingDINO installer 추가

--- sd_bmab/util.py
+++ sd_bmab/util.py
@@ -307,8 +307,10 @@ def generate_noise(seed, width, height):
 def lazy_loader(filename):
 	bmab_model_path = os.path.join(models_path, "bmab")
 	files = glob.glob(bmab_model_path)
-	
+
 	targets = {
+		'GroundingDINO_SwinT_OGC.py': 'https://github.com/IDEA-Research/GroundingDINO/blob/main/groundingdino/config/GroundingDINO_SwinT_OGC.py',
+		'groundingdino_swint_ogc.pth': 'https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth',
 		'sam_vit_b_01ec64.pth': 'https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth',
 		'sam_vit_l_0b3195.pth': 'https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth',
 		'sam_vit_h_4b8939.pth': 'https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth',
