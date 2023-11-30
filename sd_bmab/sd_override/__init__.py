9f165398eca9a15e8bdc881554c1e9ebde9bfa71
portu-sim <safecode@naver.com>
2023-11-30 12:38:01 +0900

    ### v23.11.30.0
    * Code Fix
      * Support BMAB DR

--- sd_bmab/sd_override/__init__.py
+++ sd_bmab/sd_override/__init__.py
@@ -1,8 +1,10 @@
 from sd_bmab.sd_override.samper import override_samplers
+from sd_bmab.sd_override.img2img import StableDiffusionProcessingImg2ImgOv
 from sd_bmab.sd_override.txt2img import StableDiffusionProcessingTxt2ImgOv
 
 from modules import processing
 
+processing.StableDiffusionProcessingImg2Img = StableDiffusionProcessingImg2ImgOv
 processing.StableDiffusionProcessingTxt2Img = StableDiffusionProcessingTxt2ImgOv
 
 
