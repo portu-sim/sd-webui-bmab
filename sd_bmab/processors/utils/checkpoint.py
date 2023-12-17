dfdb1c53ce6f31a01420f97146d46c7ec1516e84
ZRGX <139933536+RGX650@users.noreply.github.com>
2023-12-17 01:13:50 +0100

    add self.loaded_vae_file

--- sd_bmab/processors/utils/checkpoint.py
+++ sd_bmab/processors/utils/checkpoint.py
@@ -11,10 +11,12 @@ class CheckPointChanger(ProcessorBase):
 		super().__init__()
 		self.checkpoint = constants.checkpoint_default
 		self.vae = constants.vae_default
+		self.loaded_vae_file = None  # Initialize loaded_vae_file attribute
 
 	def preprocess(self, context: Context, image: Image):
 		self.checkpoint = context.args['preprocess_checkpoint']
 		self.vae = context.args['preprocess_vae']
+	        self.loaded_vae_file = context.args.get('loaded_vae_file')
 		return not (self.checkpoint == constants.checkpoint_default and self.checkpoint == constants.vae_default)
 
 	def process(self, context: Context, image: Image):
