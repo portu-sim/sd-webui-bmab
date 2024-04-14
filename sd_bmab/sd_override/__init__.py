ab62622a0361d6d59c40010088ffbfa3ac2260f2
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-04-14 15:57:28 +0200

    Update __init__.py by VLAD
    ExtraNoiseParams, extra_noise_callback
    implemented by Vlad after port version

--- sd_bmab/sd_override/__init__.py
+++ sd_bmab/sd_override/__init__.py
@@ -4,16 +4,16 @@ from sd_bmab.sd_override.txt2img import StableDiffusionProcessingTxt2ImgOv
 from modules import processing
 
 
-from modules import script_callbacks
-from . import script_callbacks_extranoise
+#from modules import script_callbacks
+#from . import script_callbacks_extranoise
 
 # Merge callback_map from script_callbacks_extranoise into script_callbacks
-if 'callbacks_extra_noise' not in script_callbacks.callback_map:
-    script_callbacks.callback_map['callbacks_extra_noise'] = []
+#if 'callbacks_extra_noise' not in script_callbacks.callback_map:
+    #script_callbacks.callback_map['callbacks_extra_noise'] = []
 
 # Add callbacks to the 'callbacks_extra_noise' section
-def on_extra_noise(callback):
-    script_callbacks.add_callback(script_callbacks.callback_map['callbacks_extra_noise'], callback)
+#def on_extra_noise(callback):
+    #script_callbacks.add_callback(script_callbacks.callback_map['callbacks_extra_noise'], callback)
 	
 # Now 'on_extra_noise' can be used to add callbacks for 'callbacks_extra_noise'
 
