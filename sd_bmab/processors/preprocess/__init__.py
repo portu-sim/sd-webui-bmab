3a87862b88b9cf6bd6216a777ecdfca392ac2a18
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-04-14 03:36:36 +0200

    Update __init__.py from commit 293fccf

--- sd_bmab/processors/preprocess/__init__.py
+++ sd_bmab/processors/preprocess/__init__.py
@@ -2,4 +2,5 @@ from sd_bmab.processors.preprocess.resize import ResizeIntermidiate
 from sd_bmab.processors.preprocess.refiner import RefinerPreprocessor
 from sd_bmab.processors.preprocess.pretraining import PretrainingDetailer
 from sd_bmab.processors.preprocess.resample import ResamplePreprocessor
+from sd_bmab.processors.preprocess.preprocessfilter import PreprocessFilter
 
