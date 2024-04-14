b0d45f447208402210f312e36d71a95ff85b2fc4
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-04-14 15:15:16 +0200

    Update img2img.py from commit ffe3379

--- sd_bmab/sd_override/img2img.py
+++ sd_bmab/sd_override/img2img.py
@@ -10,6 +10,12 @@ class StableDiffusionProcessingImg2ImgOv(StableDiffusionProcessingImg2Img):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def init(self, all_prompts, all_seeds, all_subseeds):
+        ret = super().init(all_prompts, all_seeds, all_subseeds)
+        self.extra_generation_params['Hires prompt'] = ''
+        self.extra_generation_params['Hires negative prompt'] = ''
+        return ret
+    
     def sample(self, conditioning, unconditional_conditioning, seeds, subseeds, subseed_strength, prompts):
         return super().sample(conditioning, unconditional_conditioning, seeds, subseeds, subseed_strength, prompts)
 
