d4ed4df32ac095fc3d55e598dd50205cd85ad412
portu.sim <portu.sim.kr@gmail.com>
2024-04-15 23:52:21 +0900

    * Support : basicsr==1.4.2 강제 설치하도록 수정

--- install.py
+++ install.py
@@ -13,8 +13,10 @@ def install_ultralytics():
     launch.run_pip('install ultralytics')
 
 
-launch.run_pip('install basicsr==1.4.2')
-
+try:
+    from basicsr.utils.download_util import load_file_from_url
+except:
+    launch.run_pip('install basicsr==1.4.2')
 
 required = {
     ('segment_anything', install_segmentanything),
